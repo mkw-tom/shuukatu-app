905addec0e3b114dded42bb7129a26ae254e07de
Tomu Maekawa <tomu.m1107@icloud.com>
2024-09-06 16:38:16 +0900

    create layout conmponents

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,44 +2,46 @@ import { Addchart, Business, MenuBook } from '@mui/icons-material'
 import Button from '@mui/material/Button'
 import Image from 'next/image'
 import Link from 'next/link'
+import SideBar from '../SIdebar/SideBar'
 import DarkModeButton from './DarkModeButton'
 import ProfileButton from './ProfileButton'
 
 const Header = () => {
   return (
-    <div className="h-16 w-full border-b-2 border-t-4 border-t-blue-600">
+    <div className="h-16 w-full border-b-2 border-t-4 border-t-blue-600 dark:border-b-gray-500">
       <div className="mx-5 flex h-16 items-center justify-between">
+        <SideBar />
         <h1 className="flex items-center">
           <Image src="/file.png" width={55} height={55} alt="ユーザー画像" />
-          <p className="bg-gradient-to-r from-sky-500 to-orange-500 bg-clip-text text-lg font-bold text-transparent ">
-            ENTRIX
+          <p className="mr-1 bg-gradient-to-r from-sky-500 to-orange-500 bg-clip-text text-xl font-bold tracking-widest text-transparent">
+            Entrix
           </p>
         </h1>
         <div className="flex items-center gap-16">
-          <nav className="flex gap-5">
+          <nav className="hidden gap-5 md:flex ">
             <Link href="/">
-              <Button className="font-bold dark:text-white" startIcon={<Business />}>
+              <Button className="font-bold " startIcon={<Business />}>
                 企業管理
               </Button>
             </Link>
             <Link href="/">
-              <Button className="font-bold dark:text-white" startIcon={<MenuBook />}>
+              <Button className="font-bold " startIcon={<MenuBook />}>
                 ES管理
               </Button>
             </Link>
             <Link href="/">
-              <Button className="font-bold dark:text-white" startIcon={<Addchart />}>
+              <Button className="font-bold " startIcon={<Addchart />}>
                 チャート
               </Button>
             </Link>
           </nav>
-          <div className="flex items-center gap-5 border-l-2 pl-5">
+          <div className="flex items-center gap-5 border-l-2 pl-5 dark:border-l-gray-500">
             <DarkModeButton />
             <ProfileButton />
           </div>
           {/* <Button
             variant='outlined'
-            className='text-blue-600 font-bold border-2 border-blue-500 dark:text-white  dark:bg-blue-500 '
+            className='text-blue-600 font-bold border-2 border-blue-500   dark:bg-blue-500 '
             endIcon={<Login />}
           >
             Login
--- src/lib/darkmode/useDarkMode.tsx
+++ src/lib/darkmode/useDarkMode.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 
 type Theme = 'light' | 'dark'
 
-const useDarkMode = (): [Theme, () => void, () => void, () => void] => {
+const useDarkMode = (): [Theme, () => void, () => void] => {
   const [theme, setTheme] = useState<Theme>('light')
 
   useEffect(() => {
@@ -28,21 +28,7 @@ const useDarkMode = (): [Theme, () => void, () => void, () => void] => {
     setTheme('light')
   }
 
-  const toggleTheme = (): void => {
-    if (theme === 'light') {
-      document.documentElement.classList.remove('light')
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
-      setTheme('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-      document.documentElement.classList.add('light')
-      localStorage.setItem('theme', 'light')
-      setTheme('light')
-    }
-  }
-
-  return [theme, toggleTheme, changeDark, changeLight]
+  return [theme, changeDark, changeLight]
 }
 
 export default useDarkMode
