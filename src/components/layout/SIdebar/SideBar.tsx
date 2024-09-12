ab7578df78f947fc200052471f65a33168711432
Tomu Maekawa <tomu.m1107@icloud.com>
2024-09-12 19:36:40 +0900

    refactor sidebar

--- src/components/layout/SIdebar/SideBar.tsx
+++ src/components/layout/SIdebar/SideBar.tsx
@@ -1,14 +1,6 @@
 'use client'
 import useDarkMode from '@/lib/darkmode/useDarkMode'
-import { Addchart, Business, Logout, Menu, MenuBook } from '@mui/icons-material'
-import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
+import { Addchart, Business, MenuBook } from '@mui/icons-material'
 import Link from 'next/link'
 import { useState } from 'react'
 
@@ -16,60 +8,53 @@ const SideBar = () => {
   const [open, setOpne] = useState<boolean>(false)
   const [theme] = useDarkMode()
 
-  const sideList = [
-    { icon: <Business />, text: '企業管理', link: '/' },
-    { icon: <MenuBook />, text: 'ES管理', link: '/' },
-    { icon: <Addchart />, text: 'チャート', link: '/' },
+  const sidebarItems = [
+    { icon: <Business className="mr-auto" />, text: '企業管理', link: '/company_page' },
+    { icon: <MenuBook className="mr-auto" />, text: 'ES管理', link: '/ES_page' },
+    { icon: <Addchart className="mr-auto" />, text: 'チャート', link: '/chart_page' },
   ]
 
   return (
-    <div className="block  md:hidden">
-      <button onClick={() => setOpne(true)}>
-        <Menu className="text-blue-600" />
-      </button>
-      <Drawer open={open === true} onClose={() => setOpne(false)}>
-        <Box
-          role="presentation"
-          className="border-t-4 border-t-blue-600  dark:bg-gray-900"
-          sx={{
-            width: 250,
-            backgroundColor: `${theme === 'dark' ? '#111827' : 'white'}`,
-            height: '100%',
-          }}
-        >
-          <List>
-            {sideList.map((li, index) => (
-              <Link href={li.link} key={index}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon className="text-blue-600 dark:text-blue-400">
-                      {li.icon}
-                    </ListItemIcon>
-                    <ListItemText
-                      className="font-bold text-blue-600 dark:text-blue-400"
-                      primary={li.text}
-                    />
-                  </ListItemButton>
-                </ListItem>
-              </Link>
-            ))}
-          </List>
-          <Divider />
-          <List className=" dark:bg-gray-900">
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemIcon className="text-blue-600 dark:text-blue-400">
-                  <Logout />
-                </ListItemIcon>
-                <ListItemText
-                  className="font-bold text-blue-600 dark:text-blue-400"
-                  primary="ログアウト"
-                />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+    <div className="drawer w-auto md:hidden">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="flex-none lg:hidden">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="open sidebar"
+            className="btn btn-square btn-ghost"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block size-6 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
+        </div>
+        {/* Page content here */}
+      </div>
+      <div className="drawer-side z-50 ">
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay "></label>
+        <ul className="menu flex min-h-full w-56 flex-col items-center bg-base-100 p-4 dark:bg-gray-700">
+          {sidebarItems.map((item, index) => (
+            <Link key={index} href={item.link} className="w-full">
+              <button className="btn btn-ghost flex w-full items-center">
+                {item.icon}
+                <span className="mr-auto">{item.text}</span>
+              </button>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
