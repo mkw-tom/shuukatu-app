2b10999261d5ea3047024c8153e7685d043e61b6
Tomu Maekawa <tomu.m1107@icloud.com>
2024-09-16 23:54:08 +0900

    refacter reducer and context

--- src/app/context/usePost.tsx
+++ src/app/context/usePost.tsx
@@ -1,12 +1,17 @@
 'use client'
 import type { Dispatch, ReactNode, SetStateAction } from 'react'
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useReducer, useState } from 'react'
+import { postReducer } from '../reducer/PostReducer'
 
 interface PostContextType {
+  postsState: PostType[]
+  postsDispatch: Dispatch<Action>
   posts: PostType[]
   setPosts: Dispatch<SetStateAction<PostType[]>>
   selectPost: PostType | null
   setSelectPost: Dispatch<SetStateAction<PostType | null>>
+  selectTask: TaskType | null
+  setSelectTask: Dispatch<SetStateAction<TaskType | null>>
 }
 const postContext = createContext<PostContextType | undefined>(undefined)
 
@@ -20,13 +25,19 @@ export const usePost = () => {
 
 export const PostContextProvider = ({ children }: { children: ReactNode }) => {
   const [posts, setPosts] = useState<PostType[]>([])
+  const [postsState, postsDispatch] = useReducer(postReducer, [])
   const [selectPost, setSelectPost] = useState<PostType | null>(null)
+  const [selectTask, setSelectTask] = useState<TaskType | null>(null)
 
   const value = {
+    postsState,
+    postsDispatch,
     posts,
     setPosts,
     selectPost,
     setSelectPost,
+    selectTask,
+    setSelectTask,
   }
   return <postContext.Provider value={value}>{children}</postContext.Provider>
 }
